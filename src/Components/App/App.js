9f0747d6a944c53fce9ec414a34c589cbc0f0e9f
Matthew Rougely <matthewrougelysr@gmail.com>
2020-09-11 13:08:27 -0700

    finished public and private routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
 // import GameWindow from '../GameWindow/GameWindow';
-import LeaderBoard from '../../Routes/LeaderBoard';
-import Login from '../../Routes/Login';
-import MainGame from '../../Routes/MainGame';
-import Dashboard from '../../Routes/Dashboard';
-import Register from '../../Routes/Register';
-import Header from '../../Components/Header';
+import LeaderBoard from "../../Routes/LeaderBoard";
+import Login from "../../Routes/Login";
+import MainGame from "../../Routes/MainGame";
+import Dashboard from "../../Routes/Dashboard";
+import Register from "../../Routes/Register";
+import Header from "../../Components/Header";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import PublicRoute from "../PublicRoute/PublicRoute";
 
-import './App.scss';
+import "./App.scss";
 
 const App = () => {
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
-      <main >
-        <Route exact path="/" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/maingame" component={MainGame} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/leaderBoard" component={LeaderBoard} />
+      <main>
+        <Switch>
+          <PublicRoute exact path="/" component={Login} />
+          <PublicRoute path="/register" component={Register} />
+          <PrivateRoute path="/maingame" component={MainGame} />
+          <PrivateRoute path="/dashboard" component={Dashboard} />
+          <PrivateRoute path="/leaderBoard" component={LeaderBoard} />
+        </Switch>
       </main>
-    </div >
+    </div>
   );
 };
 
