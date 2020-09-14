0d1620943117265566d9fe0a78d3884d8c3accbb
nickthorpe71 <nickthorpe71@gmail.com>
2020-09-13 20:04:06 -0500

    adds basic leaderboard display

--- src/Routes/LeaderBoard/index.js
+++ src/Routes/LeaderBoard/index.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import convertTime from '../../Utils/convert-time';
 
 import './LeaderBoard.scss';
 
+const temp = [
+  {
+    username: 'name1',
+    elapsedTime: 143,
+    alive: false,
+  },
+  {
+    username: 'name2',
+    elapsedTime: 456,
+    alive: true,
+  },
+  {
+    username: 'name3',
+    elapsedTime: 86,
+    alive: true,
+  }
+];
+
 const LeaderBoard = () => {
+
+  const renderList = () => {
+
+    return temp.map((player, index) => {
+      let convertedTime = convertTime(player.elapsedTime);
+      return (
+        <li key={index}>
+          <p>{player.username} | {`${convertedTime.days} day(s), ${convertedTime.hours} hours ${convertedTime.mins} mins`} | {player.alive ? 'alive' : 'deceased'}</p>
+        </li>
+      );
+    });
+  };
 
   return (
     <section className='LeaderBoard'>
-      Leaderboard
+      <ul className='leader-list'>
+        {renderList()}
+      </ul>
     </section>
   );
 };
