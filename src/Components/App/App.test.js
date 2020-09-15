5fdd210117b37d8c136ef7d6837276f778ac678c
Matthew Rougely <matthewrougelysr@gmail.com>
2020-09-14 17:22:04 -0700

    new app tests and new dependancy

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,9 +1,60 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders dashboard', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/dashboard react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+import React from "react";
+import ReactDOM from "react-dom";
+import renderer from 'react-test-renderer';
+import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from '../../Context/UserContext';
+import { GameProvider } from '../../Context/GameContext';
+import App from "./App";
+
+describe(`App Component`, () => {
+    describe(`Smoke test`, () => {
+        it("should render without crashing", () => {
+
+            const div = document.createElement('div');
+
+            ReactDOM.render(
+            <BrowserRouter>
+              <UserProvider>
+                <GameProvider>
+                  <App />
+                </GameProvider>
+              </UserProvider>
+            </BrowserRouter>,
+            div);
+
+              ReactDOM.unmountComponentAtNode(div);
+          });
+
+    })
+
+    describe(`Snapshot test`, () => {
+        it(`Renders the UI as expected`, () => {
+
+          const div = document.createElement('div');
+
+            const tree = renderer
+                .create(
+                  <BrowserRouter>
+                  <UserProvider>
+                    <GameProvider>
+                      <App />
+                    </GameProvider>
+                  </UserProvider>
+                </BrowserRouter>)
+                .toJSON()
+            expect(tree).toMatchSnapshot()
+        });
+    });
+
+})
+
+
