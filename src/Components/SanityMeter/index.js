69ff5b7447e038674856d1fdd57e7325cdf046da
Chrissy Harmon <clio.christine.harmon@gmail.com>
2020-09-17 15:31:36 -0400

    "basic meter stuff"

--- src/Components/SanityMeter/index.js
+++ src/Components/SanityMeter/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React from "react";
 
 import './SanityMeter.scss';
 
 const SanityMeter = (props) => {
+  const { bgcolor, sanity } = props;
+
+  const fillerStyles = {
+    height: '100%',
+    width: `${sanity}%`,
+    backgroundColor: bgcolor,
+    textAlign: 'right'
+  }
 
   return (
-    <p>{`S:${props.currentSanity}`}</p>
-  );
-};
+    //entire bar
+    //completed percentage
+    <div className="barContainer pixel-borders">
+      <div style={fillerStyles}>
+        <span className="barLabel">{`${sanity}`}</span>
+      </div>
+    </div>
+  )
+}
 
 export default SanityMeter;
