c3689d79366fa37aad76946b744ea8641841ba45
Chrissy Harmon <clio.christine.harmon@gmail.com>
2020-09-18 13:03:14 -0400

    playsinline

--- src/Components/StartCutscene/StartCutscene.js
+++ src/Components/StartCutscene/StartCutscene.js
@@ -9,7 +9,7 @@ const StartCutscene = () => {
   return (
     <section>
       <div className="fullscreen-bg">
-        <video autoPlay={true} muted loop src={cutscene_start} className="fullscreen-bg__video" type="video.mp4" />
+        <video playsinline autoPlay={true} muted loop src={cutscene_start} className="fullscreen-bg__video" type="video.mp4" />
         <p>click to start</p>
       </div>
       <button className="startButton" type="button" onClick={() => userContext.setStartClicked(true)}></button>
