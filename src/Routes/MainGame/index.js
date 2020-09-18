3da1cde188ee608c5cc5611de6dd02ad8b3a3aa5
nickthorpe71 <nickthorpe71@gmail.com>
2020-09-18 12:37:06 -0500

    adds fix for reload on game screen

--- src/Routes/MainGame/index.js
+++ src/Routes/MainGame/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from "react-router-dom";
+import GameContext from '../../Context/GameContext';
 import GameWindow from '../../Components/GameWindow/GameWindow';
 
 import './MainGame.scss';
 
 const MainGame = () => {
   //if the player reloads the game basically breaks so we need to bring them back to dashboard on reload
+  const context = useContext(GameContext);
+  const history = useHistory();
+
+  if (context.gameData.saved_game_id === 0) {
+    context.saveGame();
+    history.push('/dashboard');
+  }
 
   return (
     <section className='MainGame'>
